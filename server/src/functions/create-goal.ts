3374f40582ec4139ac246c6852e958c6e5e1904b
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-09-12 02:06:06 -0300

    created createGoal function

---
+++ server/src/functions/create-goal.ts
@@ -0,0 +1,30 @@
+import { db } from "../db";
+import { goals } from "../db/schema";
+
+interface ICreateGoalRequest {
+  title: string;
+  desiredWeeklyFrequency: number;
+}
+
+const createGoal = async ({
+  title,
+  desiredWeeklyFrequency,
+}: ICreateGoalRequest) => {
+  const result = await db
+    .insert(goals)
+    .values({
+      title,
+      desiredWeeklyFrequency,
+    })
+    .returning();
+  // Preciso adicionar o returning(), pois, por padrão, os db's não retornam os dados inseridos, somente, no máximo, os dados afetados. 
+  // Usando o returning eu consigo ter acesso a eles
+
+  const goal = result[0];
+  // A inserção de novos dados, por mais que adicionamos somente um, vai sempre retornar um array. 
+  // Portanto precisamos colocar o index 0 para coletar o dado.
+
+  return {
+    goal,
+  };
+};
