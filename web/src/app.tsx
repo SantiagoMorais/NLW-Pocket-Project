f1e9ee46c54e5139eee846e6195a39b3bc34ef72
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-09-15 21:00:44 -0300

    configured tailwind css and import aliases

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,6 +1,9 @@
+import logo from "@assets/in-orbit-logo.svg";
+
 export function App() {
   return (
-    <>
-    </>
-  )
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="" />
+    </div>
+  );
 }
