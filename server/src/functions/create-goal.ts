f6822b1072fd224891388d8c17ebe36aee6627d3
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-09-22 17:33:06 -0300

    created create-goal verification to avoid duplicated goals

--- server/src/functions/create-goal.ts
+++ server/src/functions/create-goal.ts
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm";
 import { db } from "../db";
 import { goals } from "../db/schema";
 
@@ -10,6 +11,16 @@ export const createGoal = async ({
   title,
   desiredWeeklyFrequency,
 }: ICreateGoalRequest) => {
+  const goalAlreadyExist = await db
+    .select()
+    .from(goals)
+    .where(eq(goals.title, title))
+    .limit(1);
+
+  if (goalAlreadyExist[0]) {
+    throw new Error("This goal already exists");
+  }
+
   const result = await db
     .insert(goals)
     .values({
@@ -17,11 +28,11 @@ export const createGoal = async ({
       desiredWeeklyFrequency,
     })
     .returning();
-  // Preciso adicionar o returning(), pois, por padrão, os db's não retornam os dados inseridos, somente, no máximo, os dados afetados. 
+  // Preciso adicionar o returning(), pois, por padrão, os db's não retornam os dados inseridos, somente, no máximo, os dados afetados.
   // Usando o returning eu consigo ter acesso a eles
 
   const goal = result[0];
-  // A inserção de novos dados, por mais que adicionamos somente um, vai sempre retornar um array. 
+  // A inserção de novos dados, por mais que adicionamos somente um, vai sempre retornar um array.
   // Portanto precisamos colocar o index 0 para coletar o dado.
 
   return {
